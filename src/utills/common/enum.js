b8fea2e9c9da4141c7502522c6485e04d15938bf
Sheral123 <sheralsingla18@gmail.com>
2024-06-06 13:31:59 +0530

    2nd

---
+++ src/utills/common/enum.js
@@ -0,0 +1,20 @@
+const SEAT_TYPE = {
+    BUSINESS: 'business',
+    PREMIUM_ECONOMY: 'premium-economy',
+    ECONOMY: 'economy',
+    FIRST_CLASS: 'first-class'
+}
+
+const BOOKING_STATUS = {
+    BOOKED: 'booked',
+    CANCALLED: 'cancelled',
+    INITIATED: 'initiated',
+    PENDING: 'pending'
+}
+
+
+
+module.exports = {
+    SEAT_TYPE,
+    BOOKING_STATUS
+}
